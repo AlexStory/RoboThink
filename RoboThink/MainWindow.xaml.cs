48dd9741fe6499dac8659764736ca157bf220777
Alex Story <astory11@yahoo.com>
2015-02-05 21:05:44 -0600

    Initial State and tests

---
+++ RoboThink/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoboThink {
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window {
+    public MainWindow() {
+      InitializeComponent();
+      SelectedTab = "NSS";
+    }
+    public SolidColorBrush selected = new SolidColorBrush(Color.FromArgb((byte)100,(byte)221,(byte)221,(byte)221));
+    public SolidColorBrush notSelected = new SolidColorBrush(Color.FromArgb((byte)255, (byte)100, (byte)100, (byte)100));
+    
+    public string SelectedTab;
+
+
+    private void VIM_Click(object sender, RoutedEventArgs e) {
+      changeTab(VIM);
+    }
+
+    private void changeTab(Button obj) {
+      NSS.BorderBrush = notSelected;
+      VIM.BorderBrush = notSelected;
+      GIT.BorderBrush = notSelected;
+      obj.BorderBrush = selected;
+      SelectedTab = obj.Content.ToString();
+    }
+
+    private void NSS_Click(object sender, RoutedEventArgs e) {
+      changeTab(NSS);
+    }
+
+    private void GIT_Click(object sender, RoutedEventArgs e) {
+      changeTab(GIT);
+    }
+  }
+}
---
+++ UnitTestProject1/ZeroStateTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.Factory;
+using TestStack.White.UIItems.WindowItems;
+using RoboThink;
+using System.IO;
+
+namespace UnitTestProject1 {
+  [TestClass]
+  public class UITests {
+    private static TestContext test_context;
+    private static Window window;
+    private static Application application;
+
+    [ClassInitialize]
+    public static void Setup(TestContext _context) {
+      test_context = _context;
+      var applicationDir = _context.DeploymentDirectory;
+      var applicationPath = Path.Combine(applicationDir, "..\\..\\..\\UnitTestProject1\\bin\\Debug\\RoboThink");
+      application = Application.Launch(applicationPath);
+      window = application.GetWindow("MainWindow", InitializeOption.NoCache);
+
+    }
+
+    [TestMethod]
+
+
+    public void TestZeroState() {
+      Button NSSButton = window.Get<Button>("NSS");
+      Button VIMButton = window.Get<Button>("VIM");
+
+            string SelectedTab ="NSS";
+
+
+      Assert.AreEqual(SelectedTab, "NSS");
+      Assert.IsTrue(NSSButton.Enabled);
+      Assert.IsTrue(VIMButton.Enabled);
+
+    }
+
+    [ClassCleanup]
+    public static void TearDown() {
+      window.Close();
+      application.Close();
+    }
+  }
+}
